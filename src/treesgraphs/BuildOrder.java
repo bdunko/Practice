99ba87ca3d92f239c8358e468428ea9e60041e73
bdunko <bdunko@gmail.com>
2022-07-01 17:35:18 -0400

    improved buildorder

--- src/treesgraphs/BuildOrder.java
+++ src/treesgraphs/BuildOrder.java
@@ -1,6 +1,8 @@
 package treesgraphs;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
@@ -9,33 +11,21 @@ import datastructures.BGraph;
 import datastructures.BGraphNode;
 import testing.Test;
 
+//Given a list of projects and a list of dependency (where each dependency is represented as a pair of projects such that the second project depends on the first)
+//Find and return an order that allows all projects to be built, or null if no order exists
 public class BuildOrder {
-
-	private static void addVertices(BGraph<String> graph, String projects) {
-		String[] projectNames = projects.split(", ");
-		for(String projectName : projectNames) {
-			graph.nodes.add(new BGraphNode<String>(projectName));
-		}
-	}
 	
-	private static void addEdges(BGraph<String> graph, String dependencies) {
-		String[] pairs = dependencies.split("\\), ");
-		for(String pair : pairs) {
-			String[] vertices = pair.split(", ");
-			BGraphNode<String> project = graph.getVertex(vertices[0].replace("(", ""));
-			BGraphNode<String> dependency = graph.getVertex(vertices[1].replace(")", ""));
-			project.neighbors.add(dependency);
-		}
-	}
-	
-	public static List<BGraphNode<String>> buildOrder(String projects, String dependencies) {
+	//Solution using graphs.
+	//Represent the problem as a graph, where each node is a project
+	//And each edge is a dependency
+	public static List<String> buildOrder(String projects, String dependencies) {
 		//construct graph representation of problem
 		BGraph<String> graph = new BGraph<String>();
 		addVertices(graph, projects);
 		addEdges(graph, dependencies);
 		int graphSize = graph.nodes.size();
 		
-		List<BGraphNode<String>> buildOrder = new LinkedList<BGraphNode<String>>();
+		List<String> buildOrder = new LinkedList<String>();
 		Set<BGraphNode<String>> used = new HashSet<BGraphNode<String>>();
 		
 		//attempt to find a node 
@@ -60,7 +50,7 @@ public class BuildOrder {
 				
 				//add that node to the build order
 				used.add(currentNode);
-				buildOrder.add(currentNode);
+				buildOrder.add(currentNode.data);
 				circular = false;
 				break;
 			}
@@ -73,17 +63,85 @@ public class BuildOrder {
 		return buildOrder;
 	}
 	
-	private static <T> boolean aBeforeB(List<BGraphNode<T>> list, T a, T b) {
-		int aIndex = -1;
-		int bIndex = -1;
-		
-		for(int i = 0; i < list.size(); i++) {
-			BGraphNode<T> node = list.get(i);
-			if(node.data.equals(a))
-				aIndex = i;
-			else if (node.data.equals(b))
-				bIndex = i;
+	//Helper method that assists in constructing the graph by parsing an input string
+	private static void addVertices(BGraph<String> graph, String projects) {
+		String[] projectNames = projects.split(", ");
+		for(String projectName : projectNames) {
+			graph.nodes.add(new BGraphNode<String>(projectName));
 		}
+	}
+	
+	//Helper method that assists in constructing the graph by parsing an input string
+	private static void addEdges(BGraph<String> graph, String dependencies) {
+		String[] pairs = dependencies.split("\\), ");
+		for(String pair : pairs) {
+			String[] vertices = pair.split(", ");
+			BGraphNode<String> project = graph.getVertex(vertices[0].replace("(", ""));
+			BGraphNode<String> dependency = graph.getVertex(vertices[1].replace(")", ""));
+			project.neighbors.add(dependency);
+		}
+	}
+	
+	//More efficient version that uses a HashMap
+	//Keys in the HashMap are projects. Value is a HashSet containing all dependencies for the project.
+	//Then we can iterate over the set of keys and find projects with no dependencies, build them
+	//And remove them as dependencies from other projects
+	//Repeating until a circular dependency is found or all projects have been built
+	private static List<String> buildOrderV2(String projects, String dependencies) {
+		HashMap<String, HashSet<String>> dependencyMap = new HashMap<String, HashSet<String>>();
+		
+		//parse project names
+		for(String projectName : projects.split(", ")) {
+			dependencyMap.put(projectName, new HashSet<String>());
+		}
+		
+		//parse dependencies
+		for(String pair : dependencies.split("\\), ")) {
+			String[] projectPair = pair.split(", ");
+			String project = projectPair[1].replace(")", "");
+			String dependency = projectPair[0].replace("(", "");
+			dependencyMap.get(project).add(dependency);
+		}
+		
+		List<String> buildOrder = new LinkedList<String>();
+		
+		//until all projects have been added to build order...
+		while(dependencyMap.size() > 0) {
+			boolean circular = true;
+			
+			//find a project with zero dependencies
+			Iterator<String> iter = dependencyMap.keySet().iterator();
+			while(iter.hasNext()) {
+				String project = iter.next();
+				if(dependencyMap.get(project).size() == 0) {
+					//add that project to build order
+					buildOrder.add(project);
+					
+					//remove this project from set of projects left to build 
+					iter.remove();
+					circular = false;
+					
+					//remove this project from dependencies of each other project
+					Iterator<String> iter2 = dependencyMap.keySet().iterator();
+					while(iter2.hasNext()) {
+						dependencyMap.get(iter2.next()).remove(project);
+					}
+				}
+			}
+			
+			//if, after checking all projects, there are none we can build
+			//there are circular dependencies; return null
+			if(circular)
+				return null;
+		}
+		
+		return buildOrder;
+	}
+	
+	//Testing helper method that checks if a given element comes before another element in a list
+	private static <T> boolean aBeforeB(List<T> list, T a, T b) {
+		int aIndex = list.indexOf(a);
+		int bIndex = list.indexOf(b);
 		
 		if(aIndex == -1 || bIndex == -1)
 			return false;
@@ -113,14 +171,30 @@ public class BuildOrder {
 		Test.assertion(graph.getVertex("d").hasNeighbor("c"));
 		
 		Test.header("buildOrder");
-		List<BGraphNode<String>> buildOrder = buildOrder("a, b, c, d, e, f", "(a, d), (f, b), (b, d), (f, a), (d, c)");
-		System.out.println(buildOrder);
+		List<String> buildOrder = buildOrder("a, b, c, d, e, f", "(a, d), (f, b), (b, d), (f, a), (d, c)");
 		Test.assertion(buildOrder.size() == 6);
 		Test.assertion(aBeforeB(buildOrder, "a", "d"));
 		Test.assertion(aBeforeB(buildOrder, "f", "b"));
 		Test.assertion(aBeforeB(buildOrder, "b", "d"));
 		Test.assertion(aBeforeB(buildOrder, "f", "a"));
 		Test.assertion(aBeforeB(buildOrder, "d", "c"));
+		
+		Test.header("buildOrderV2");
+		buildOrder = buildOrderV2("a, b, c, d, e, f", "(a, d), (f, b), (b, d), (f, a), (d, c)");
+		Test.assertion(buildOrder.size() == 6);
+		Test.assertion(aBeforeB(buildOrder, "a", "d"));
+		Test.assertion(aBeforeB(buildOrder, "f", "b"));
+		Test.assertion(aBeforeB(buildOrder, "b", "d"));
+		Test.assertion(aBeforeB(buildOrder, "f", "a"));
+		Test.assertion(aBeforeB(buildOrder, "d", "c"));
+		
+		Test.isNull(buildOrderV2("a, b, c, d, e, f", "(a, d), (f, b), (b, d), (f, a), (d, c), (a, a)"));
+		Test.isNull(buildOrderV2("a, b, c", "(a, b), (b, c), (c, a)"));
+		
+		buildOrder = buildOrderV2("a, b, c, d", "(a, b), (b, c)");
+		Test.assertion(buildOrder.size() == 4);
+		Test.assertion(aBeforeB(buildOrder, "a", "b"));
+		Test.assertion(aBeforeB(buildOrder, "b", "c"));
 		
 		Test.results();
 	}
